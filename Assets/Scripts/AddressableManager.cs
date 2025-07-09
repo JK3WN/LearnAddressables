3fbe01013b7b2c6e29a1623d4ede245dd7962634
JK3WN <yimjay1@gmail.com>
2025-07-09 12:39:23 +0900

    Implemented Local Addressables

---
+++ Assets/Scripts/AddressableManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressableManager : MonoBehaviour
+{
+    [SerializeField] private AssetReferenceGameObject characterObj;
+
+    private List<GameObject> gameObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        StartCoroutine(InitAddressable());
+    }
+
+    IEnumerator InitAddressable()
+    {
+        var init = Addressables.InitializeAsync();
+        yield return init;
+    }
+
+    public void Button_SpawnObject()
+    {
+        characterObj.InstantiateAsync().Completed += (obj) =>
+        {
+            gameObjects.Add(obj.Result);
+        };
+    }
+
+    public void Button_Release()
+    {
+        if (gameObjects.Count == 0) return;
+        var idx = gameObjects.Count - 1;
+        Addressables.ReleaseInstance(gameObjects[idx]);
+    }
+}
